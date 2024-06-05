2866effc073affae0909080eaa0434da9a459673
Ekpa <ekpa.ntan@gmail.com>
2024-06-05 23:58:06 +0100

    download

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -5,11 +5,19 @@ import AppContext from "@/components/Provider";
 import { ReactEventHandler, ReactHTMLElement } from "react";
 import { useState } from "react";
 import { db } from '../../firebase/index'
+import { CSVLink } from "react-csv";
 import { collection, addDoc, getDocs, limit, query, where, doc, updateDoc, setDoc, getDoc, startAt, startAfter, getCountFromServer, serverTimestamp, endBefore, onSnapshot } from "firebase/firestore";
 
 export default function Registration() {
     const [formData, setFormData] = useState<{ name: string, center: string, phone: string }>({ name: "", center: "", phone: "" })
     const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [attendance, setAttendance] = useState<any>([])
+
+    const headers = [
+        { label: "Name", key: "name" },
+        { label: "Center", key: "center" },
+        { label: "Phone", key: "phone" },
+    ]
 
     async function handleSubmit(e: any) {
         e.preventDefault()
@@ -19,13 +27,36 @@ export default function Registration() {
             center: formData.center,
             phone: formData.phone
         });
-        
+
         setFormData({ name: "", center: "", phone: "" })
         setIsLoading(false)
     }
 
-    function handleChange(e:any) {
-        setFormData({...formData, [e.target.name]:e.target.value})
+    function addSpaceEveryThreeCharacters(str: string) {
+        let result = '';
+        
+        for (let i = 0; i < str.length; i++) {
+          result += str[i];
+          if ((i + 1) % 3 === 0 && i + 1 !== str.length) {
+            result += ' ';
+          }
+        }
+        
+        return result;
+      }
+
+    async function download() {
+        const querySnapshot = await getDocs(collection(db, "registration"));
+        const data: any = []
+        querySnapshot.forEach((doc) => {
+            data.push({ name: doc.data().name, center: doc.data().center, phone: doc.data().phone })
+        });
+        setAttendance(data)
+        // debugger      
+    }
+
+    function handleChange(e: any) {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
     return (
         <AppContext.Consumer>
@@ -34,13 +65,30 @@ export default function Registration() {
                     <main className="flex min-h-screen w-[100%] items-start justify-center gap-[20px] p-4">
                         <form className="flex w-[400px] borde p-2 flex-col gap-[30px]">
                             <label htmlFor="name">NAME</label>
-                            <input onChange={(e)=>{handleChange(e)}} value={formData.name} name="name" className="outline-none border w-full" />
+                            <input onChange={(e) => { handleChange(e) }} value={formData.name} name="name" className="outline-none border w-full" />
                             <label htmlFor="center">CENTER</label>
-                            <input onChange={(e)=>{handleChange(e)}} value={formData.center} name="center" className="outline-none border w-full" />
+                            <input onChange={(e) => { handleChange(e) }} value={formData.center} name="center" className="outline-none border w-full" />
                             <label htmlFor="phone">PHONE</label>
-                            <input onChange={(e)=>{handleChange(e)}} value={formData.phone} name="phone" className="outline-none border w-full" />
-                            <button disabled={isLoading} className="border p-4 w-fit self-center rounded" onClick={(e)=>{handleSubmit(e)}}>{isLoading ? "Uploading" : "Submit"}</button>
+                            <input onChange={(e) => { handleChange(e) }} value={formData.phone} name="phone" className="outline-none border w-full" />
+                            <button disabled={isLoading} className="border p-4 w-fit self-center rounded" onClick={(e) => { handleSubmit(e) }}>{isLoading ? "Uploading" : "Submit"}</button>
                         </form>
+                        <CSVLink
+                            filename="singles-hangout-attendance.csv"
+                            data={attendance}
+                            headers={headers}
+                            asyncOnClick={true}
+                            onClick={async (event, done) => {
+                                const querySnapshot = await getDocs(collection(db, "registration"));
+                                const data: any = []
+                                querySnapshot.forEach((doc) => {
+                                    data.push({ name: doc.data().name, center: doc.data().center, phone: addSpaceEveryThreeCharacters(doc.data().phone.toString()) })
+                                });
+                                setAttendance(data)
+                                done()
+                            }}
+                        >
+                            Download Attendance
+                        </CSVLink>
                     </main>
                 )
             }}
