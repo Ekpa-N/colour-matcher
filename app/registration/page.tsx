a9bd713045ec79b38d9964e6c4f014e6cf8529b6
Ekpa <ekpa.ntan@gmail.com>
2024-09-13 23:37:37 +0100

    email added

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -9,7 +9,7 @@ import { CSVLink } from "react-csv";
 import { collection, addDoc, getDocs, limit, query, where, doc, updateDoc, setDoc, getDoc, startAt, startAfter, getCountFromServer, serverTimestamp, endBefore, onSnapshot } from "firebase/firestore";
 
 export default function Registration() {
-    const [formData, setFormData] = useState<{ name: string, center: string, phone: string }>({ name: "", center: "", phone: "" })
+    const [formData, setFormData] = useState<{ name: string, center: string, phone: string, email:"" }>({ name: "", center: "", phone: "", email:"" })
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [attendance, setAttendance] = useState<any>([])
 
@@ -25,10 +25,11 @@ export default function Registration() {
         await setDoc(doc(db, "registration", formData.phone), {
             name: formData.name,
             center: formData.center,
-            phone: formData.phone
+            phone: formData.phone,
+            email: formData.email
         });
 
-        setFormData({ name: "", center: "", phone: "" })
+        setFormData({ name: "", center: "", phone: "", email: "" })
         setIsLoading(false)
     }
 
@@ -63,17 +64,19 @@ export default function Registration() {
             {({ defaultPattern, defaultPatterns, playerPatterns, makeSwitch, makeSwitchToo, playerOneSwitch, playerTwoSwitch, resetDefault, playerOnePattern, playerTwoPattern, playerOnePatternError }) => {
                 return (
                     <main className="flex flex-col min-h-screen w-[100%] items-start justify-center gap-[20px] p-4">
-                        <form className="flex w-[400px] borde p-2 flex-col gap-[30px]">
+                        <form className="flex w-[400px] border m-auto borde p-2 flex-col gap-[30px]">
                             <label htmlFor="name">NAME</label>
                             <input onChange={(e) => { handleChange(e) }} value={formData.name} name="name" className="outline-none border w-full" />
                             <label htmlFor="center">CENTER</label>
                             <input onChange={(e) => { handleChange(e) }} value={formData.center} name="center" className="outline-none border w-full" />
                             <label htmlFor="phone">PHONE</label>
                             <input onChange={(e) => { handleChange(e) }} value={formData.phone} name="phone" className="outline-none border w-full" />
+                            <label htmlFor="phone">Email</label>
+                            <input onChange={(e) => { handleChange(e) }} value={formData.email} name="email" className="outline-none border w-full" />
                             <button disabled={isLoading} className="border p-4 w-fit self-center rounded" onClick={(e) => { handleSubmit(e) }}>{isLoading ? "Uploading" : "Submit"}</button>
                         </form>
                         <CSVLink
-                            filename="singles-hangout-attendance.csv"
+                            filename="attendance.csv"
                             data={attendance}
                             headers={headers}
                             asyncOnClick={true}
